f267f3745bfc1280e6d3e3bf8b0a8723120f61de
agus-dc <decarloagustin@gmail.com>
2025-02-18 07:17:21 -0300

    input request validations

--- src/main/java/com/challenge/client/dto/in/ClientRequest.java
+++ src/main/java/com/challenge/client/dto/in/ClientRequest.java
@@ -1,20 +1,33 @@
 package com.challenge.client.dto.in;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.ZonedDateTime;
+
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClientRequest {
+
+    @NotEmpty(message = "Name may not be empty")
+    @Size(min = 2, max = 32, message = "Name must be between 2 and 32 characters long")
     String name;
+
+    @NotEmpty(message = "Name may not be empty")
+    @Size(min = 2, max = 32, message = "Name must be between 2 and 32 characters long")
     String surname;
+
+    @Min(value = 1, message = "Age should not be less than 1")
+    @Max(value = 150, message = "Age should not be greater than 150")
     Integer age;
+
+    @NotNull(message = "Born date not be null")
     @JsonProperty("born_date")
     ZonedDateTime bornDate;
 }
